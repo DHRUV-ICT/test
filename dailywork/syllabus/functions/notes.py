53779bb7f363c8f51addb494d0c6362d888c2d30
dhruv <patel.dhruv@icreativetechnolabs.com>
2022-02-11 17:14:21 +0530

    [ADD]

---
+++ dailywork/syllabus/functions/notes.py
@@ -0,0 +1,7 @@
+# whenever we dont pass type in prnt whenb  we asiign function/keywods to var
+# it give type of that
+x = True      # function print,
+print(x)  # keywords True,False,
+print(True + True + False) # datatypes int,str,float
+#if,else.elif.for,while all are keywords to strat loops
+
---
+++ dailywork/syllabus/functions/practice.py
@@ -0,0 +1,36 @@
+
+
+#def func(x=4,y):  #we cant give second argument as a default but first can
+# def func(x,y=4):
+#     return x,y
+# a,b = func()
+# print(a,b)
+
+
+
+# def str(a,b):
+#     print(  f' 1 is  {a} , 2 is {b} ,3 is ')
+#str(a=1,b)  #if want to add argument by name then all parameters has to give by name
+# str('b','a')
+
+# def fun(m=[]):
+#     print(m)
+# fun([1,2,3,4,5,6,7,8]) # if we pass list in function then we must be give list otherwise it change that value
+
+
+def fun():
+    pass  # it gives none type
+    return 1,2,3,4
+print(type(fun()))  # it gives class tuple
+
+#Positional arguments
+#must agree in order and number with the parameters declared in the function definition.
+#funcname(1,2,3)
+
+#Keyword arguments
+#must agree with declared parameters in number, but they may be specified in arbitrary order.
+#functoinname(a=1,b=2,c=3)
+
+#Default parameters
+#allow some arguments to be omitted when the function is called.
+#functionname(give any one argument)
